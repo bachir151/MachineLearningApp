6971bf6997c6e0862f37dd49f339f08db87cef59
bachir151 <elbachir90@gmail.com>
2023-07-29 21:32:32 +0200

    Add files via upload

--- modules.py
+++ modules.py
@@ -110,7 +110,9 @@ def final_preprocessing(texte):
 def predict_tags (texte) :
     tfidf_vectorizer = joblib.load('tfidf_vectorizer_100_words.joblib')
     multilabel_binarizer = joblib.load("multilabel_100_words.joblib")
-    model = joblib.load("logit_tdidf_100_words.joblib")
+    #model = joblib.load("logit_tdidf_100_words.joblib")
+    model = joblib.load("rfc_final_model.joblib")
+    dict_tag =joblib.load("dict_tag.joblib")
     texte = [texte]
     texte_tfidf = tfidf_vectorizer.transform(texte)
 
